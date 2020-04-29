2339a7e8b0bb3366500a83fce1d745a52b421526
Jack Hyland <101603196@student.swin.edu.au>
2020-04-29 10:12:54 +1000

    added eight sided die

--- DiceRollerInJava.java
+++ DiceRollerInJava.java
@@ -12,19 +12,38 @@ public class DiceRollerInJava {
                            { { 0, 0, 1 }, { 0, 1, 0 }, { 1, 0, 0 } }, 
                            { { 1, 0, 1 }, { 0, 0, 0 }, { 1, 0, 1 } },
                            { { 1, 0, 1 }, { 0, 1, 0 }, { 1, 0, 1 } }, 
-                           { { 1, 0, 1 }, { 1, 0, 1 }, { 1, 0, 1 } } };
+                           { { 1, 0, 1 }, { 1, 0, 1 }, { 1, 0, 1 } },
+                           { { 1, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 } },
+                           { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } } };
  
     public static void main(String[] args) {
         int totalResultPlayer1 = 0;
+        int result;
         Scanner scanner = new Scanner(System.in);
         DiceRollerInJava dice = new DiceRollerInJava();
         while (true) {
-            int result = dice.roll();
+            int dieTemp = 6; // temp value for die 
+            
+            // procedure for rolling eight sided die
+            // prompts user if they wish to roll a eight sided die
+            // if yes, dieTemp will be set to 8 and will return a value between 1-8 when called roll(dieTemp)
+            // else will return a value between 1-6 when called roll(dieTemp)
+            System.out.println("would you like to roll a 8 sided die? (type y/n):");
+            String eightInput = scanner.nextLine();
+            if (eightInput.equalsIgnoreCase("y") || eightInput.equalsIgnoreCase("yes")){
+                dieTemp = 8;
+                result = dice.roll(dieTemp);
+                System.out.println("Rolling eight sided die!");
+            }
+            else{
+                result = dice.roll(dieTemp);
+                System.out.println("Rolling six sided die!");                         
+            }
             totalResultPlayer1 += result;
             System.out.println("dice face value:" + result);
             System.out.println("total:" + totalResultPlayer1);
             dice.draw(result);
- 
+            
             System.out.println("Roll again? (type \"no\" to quit or type \"reset\" to reset the total):");
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("n") || input.equalsIgnoreCase("no")) 
@@ -64,8 +83,8 @@ public class DiceRollerInJava {
     }
  
     // Roll the dice in Java
-    private int roll() {
+    private int roll(int value) {
         Random r = new Random();
-        return r.nextInt(6) + 1;
+        return r.nextInt(value) + 1;
     }
 }
